95afa3f10d32b783b608fd6537804d3b7bed52f0
lifeistech <ayano.060605@icloud.com>
2021-02-14 16:06:34 +0900

    commit

---
+++ Schedule/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  Schedule
+//
+//  Created by ayano on 2021/02/14.
+//  Copyright © 2021 ayano. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var Fast:UITextField!
+    @IBOutlet var Secand:UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
