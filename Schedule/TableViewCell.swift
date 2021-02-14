e5a01bde7af89c98063c015a74428691c3a0348e
lifeistech <ayano.060605@icloud.com>
2021-02-14 11:36:19 +0900

    コミット

---
+++ Schedule/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Schedule
+//
+//  Created by ayano on 2021/01/31.
+//  Copyright © 2021 ayano. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Schedule/ViewController.swift
+++ Schedule/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "tesuto"
+        
+        return cell!
+        
+    }
+    
+    @IBOutlet var table: UITableView!
+   
+    
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //calendar.appearance.todayColor = UIColor.red
+        calendar.appearance.headerTitleColor = UIColor.red
+        calendar.appearance.weekdayTextColor = UIColor.red
+
+    }
+    @IBAction func didTapButton (){
+       self.performSegue(withIdentifier: "toSecond", sender: nil)
+    
+    
     }
 
 
